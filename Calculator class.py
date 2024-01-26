dd2edfbb2a73d4682a1d499c38c5568f4d0793fd
Mianwaiz <148198099+Mianwaiz@users.noreply.github.com>
2024-01-26 22:11:01 +0500

    Create Calculator class.py
    It will calculate arithmetic operators with in class

---
+++ Calculator class.py
@@ -0,0 +1,17 @@
+#class
+class Calculator:
+	def add(self,num1,num2):
+		return num1+num2
+	def sub(self,num1,num2):
+		return num1-num2
+	def multi(self,num1,num2):
+		return num1*num2
+	def divide(self,num1,num2):
+		return num1/num2
+#objects
+calculus=Calculator()
+#operators
+add=calculus.add(1,2)
+sub=calculus.add(1,2)
+multi=calculus.add(1,2)
+divide=calculus.add(1,2)
